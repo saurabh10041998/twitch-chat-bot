7d26e5993a695a81600fc358243be3656fbb5b1f
saurabh10041998 <shindesaurabhpict@gmail.com>
2024-10-25 20:48:17 +0530

    Understanding and taking notes of the codes

---
+++ notes/notes.md
@@ -0,0 +1,7 @@
+## testing directory
+
+`pyi` files are called type stubs file
+provide type information about the modules which may not include type information in
+their main code.
+
+
--- testing/stubs/async_lru.pyi
+++ testing/stubs/async_lru.pyi
@@ -1,5 +1,9 @@
 from typing import Callable, TypeVar
 
-T = TypeVar('T')
+T = TypeVar('T')    # Here T is generic type variable
 
+# alru_cache is function which takes argument
+# of type either int or None
+# written callable entity, which in turns takes generic type T
+# returns T
 def alru_cache(maxsize: int | None) -> Callable[[T], T]: ...
